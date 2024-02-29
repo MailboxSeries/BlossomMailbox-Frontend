ce66e3aa07cba7b464be3769fe594a06f6507da6
minseong0324 <kwan0324@naver.com>
2024-03-01 06:31:22 +0900

    Style: 폴더구조, 파일명 수정

--- src/pages/SignIn/style.ts
+++ src/pages/SignIn/style.ts
@@ -4,6 +4,6 @@ export const SocialButtonWrapper = styled.div`
     align-items: center;
     display: flex;
     flex-direction: column;
-    margin-top: 650px;
-    position: absolute;
+    margin-top: 450px;
+    position: relative;
 `;
