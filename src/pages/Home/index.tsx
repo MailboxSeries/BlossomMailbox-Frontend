75b3f9e1fa18c4bed1f021481400b4fb74149285
minseong0324 <kwan0324@naver.com>
2024-03-01 06:56:12 +0900

    Remove: 불필요 코드 삭제

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import CharacterLayout from '@/components/CharacterLayout';
 import PageLayout from '@/components/PageLayout';
 import StoreLayout from '@/components/StoreLayout';
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <PageLayout>
         <StoreLayout /> {/* TODO: rightStore, leftStore 서버로 부터 받은 값 넘겨야함 */}
+        <CharacterLayout /> {/* TODO: sex, hair, face, top, bottom 서버로 부터 받은 값 넘겨야함 */}
       </PageLayout>
     </>
   );
