65d737358d32be853869ea48ecb8c2810d838354
minseong0324 <kwan0324@naver.com>
2024-03-01 08:33:57 +0900

    Feat: Home에 AnimalButton 적용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import AnimalButton from '@/components/\bAnimalButton';
 import CharacterLayout from '@/components/CharacterLayout';
 import PageLayout from '@/components/PageLayout';
 import StoreLayout from '@/components/StoreLayout';
@@ -9,6 +10,7 @@ export default function Home() {
       <PageLayout>
         <StoreLayout /> {/* TODO: rightStore, leftStore 서버로 부터 받은 값 넘겨야함 */}
         <CharacterLayout /> {/* TODO: sex, hair, face, top, bottom 서버로 부터 받은 값 넘겨야함 */}
+        <AnimalButton /> {/* TODO: animal 서버로 부터 받은 값 넘겨야함 */}
       </PageLayout>
     </>
   );
--- src/utils/getCurrentAnimalImage.ts
+++ src/utils/getCurrentAnimalImage.ts
@@ -13,6 +13,6 @@ export function getCurrentAnimalImage(animal: string): { animalImage: string } {
     };
 
     // 파라미터 값이 예상 범위를 벗어날 경우 기본 이미지(예: cat1)를 반환합니다.
-    const animalImage = animalImages[animal] || cat1;
+    const animalImage = animalImages[animal] || cat4;
     return { animalImage };
 }
