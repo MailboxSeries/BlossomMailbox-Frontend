5a42611abab9fc515531522d21d4f1aff7f7566c
minseong0324 <kwan0324@naver.com>
2024-03-02 03:54:49 +0900

    Fix: Toast 문제 개선

--- src/components/common/Toast/index.tsx
+++ src/components/common/Toast/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import * as Styled from './style';
 
 interface ToastProps {
@@ -7,11 +5,7 @@ interface ToastProps {
 }
 
 function Toast({ message }: ToastProps) {
-    // Toast 컨테이너를 toast-root에 포탈을 사용하여 렌더링합니다.
-    return ReactDOM.createPortal(
-            <Styled.ToastContainer>{message}</Styled.ToastContainer>,
-            document.getElementById('modal-root') // 이것은 모달이 렌더링 될 대상 DOM 요소입니다.
-        )
+    return <Styled.ToastContainer>{message}</Styled.ToastContainer>
 }
 
 export default Toast;
