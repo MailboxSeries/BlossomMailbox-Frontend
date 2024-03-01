d51d41cbf56c9e3a02881f863f5da170aee448d9
minseong0324 <kwan0324@naver.com>
2024-03-01 10:17:34 +0900

    Fix: 빌드에러

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import {fileURLToPath} from 'url';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: {
     rollupOptions: {
