8fb4f4fa27c37545651d24c02c2602b962169128
minseong0324 <kwan0324@naver.com>
2024-03-01 10:12:08 +0900

    Fix: 빌드에러

--- src/utils/getCurrentAnimalImage.ts
+++ src/utils/getCurrentAnimalImage.ts
@@ -4,7 +4,7 @@ import cat3 from '@/assets/animal/cat3.png';
 import cat4 from '@/assets/animal/cat4.png';
 
 /** 동물 이미지 반환하는 함수 */
-export function getCurrentAnimalImage(animal: string): { animalImage: string } {
+export function getCurrentAnimalImage(animal?: string): { animalImage?: string } {
     const animalImages = {
         cat1,
         cat2,
@@ -12,7 +12,7 @@ export function getCurrentAnimalImage(animal: string): { animalImage: string } {
         cat4,
     };
 
-    // 파라미터 값이 예상 범위를 벗어날 경우 기본 이미지(예: cat1)를 반환합니다.
+    // 파라미터 값이 예상 범위를 벗어날 경우 기본 이미지(예: cat4)를 반환합니다.
     const animalImage = animalImages[animal] || cat4;
     return { animalImage };
 }
