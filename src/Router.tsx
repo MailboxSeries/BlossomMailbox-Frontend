f7e35d3e65923e05147190810ca9dd9f17e23e46
minseong0324 <kwan0324@naver.com>
2024-03-01 20:11:18 +0900

    Fix: Redirect page 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import Redirect from './pages/Redirect';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
+      <Route path="/redirect" element={<Redirect />} />
       <Route path="/home/:ownerId" element={<Home />} />
     </Routes>
   );
---
+++ src/pages/Redirect/index.tsx
@@ -0,0 +1,13 @@
+import PageLayout from '@/components/PageLayout';
+import * as Styled from './style';
+
+export default function Home() {
+
+  return (
+    <>
+      <PageLayout>
+        <></>
+      </PageLayout>
+    </>
+  );
+}
