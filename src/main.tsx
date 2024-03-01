5897a844837a25af79e86f630a96dd595d1960b4
minseong0324 <kwan0324@naver.com>
2024-03-01 10:56:36 +0900

    Feat: PWA 업데이트 기능

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,20 @@ import {GlobalStyle} from './style';
 import {initMocks} from './mocks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // 새로운 콘텐츠가 사용 가능할 때 호출됨
+    // 여기서 사용자에게 업데이트를 알리고, 새로고침을 유도할 수 있음
+    if (confirm('새로운 버전이 사용 가능합니다. 지금 업데이트하시겠습니까?')) {
+      updateSW();
+    }
+  },
+  onOfflineReady() {
+    // PWA가 오프라인에서 사용 가능해질 때 호출됨
+  },
+});
 
 async function enableMocking() {
   if (!import.meta.env.VITE_APP_NODE_ENV) {
