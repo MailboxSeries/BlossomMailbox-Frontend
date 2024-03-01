a56465da2a829fadb9a04230cb2cd549b05fc1e8
minseong0324 <kwan0324@naver.com>
2024-03-01 10:13:53 +0900

    Fix: 빌드에러

--- src/utils/getCurrentAnimalImage.ts
+++ src/utils/getCurrentAnimalImage.ts
@@ -5,12 +5,12 @@ import cat4 from '@/assets/animal/cat4.png';
 
 /** 동물 이미지 반환하는 함수 */
 export function getCurrentAnimalImage(animal?: string): { animalImage?: string } {
-    const animalImages = {
+    const animalImages = [
         cat1,
         cat2,
         cat3,
         cat4,
-    };
+    ];
 
     // 파라미터 값이 예상 범위를 벗어날 경우 기본 이미지(예: cat4)를 반환합니다.
     const animalImage = animalImages[animal] || cat4;
