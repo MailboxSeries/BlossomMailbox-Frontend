ffb1c9e4e61498092c867790ea1fc710c6bef5ab
minseong0324 <kwan0324@naver.com>
2024-03-02 22:36:39 +0900

    Feat: 스킨 state 및 interface 정의

---
+++ src/atoms/skinState.ts
@@ -0,0 +1,16 @@
+import { ISkinState } from '@/interfaces/skinState';
+import { atom } from 'recoil';
+
+export const skinState = atom<ISkinState>({
+    key: 'skinState',
+    default: {
+        sex: "woman",
+        top: 1,
+        bottom: 1,
+        face: 1,
+        hair: 1,
+        animal: "cat4",
+        rightStore: "churros1",
+        leftStore: "balloon1",
+    },
+});
---
+++ src/interfaces/skinState.ts
@@ -0,0 +1,10 @@
+export interface ISkinState {
+    sex: "man" | "woman";
+    top: number;
+    bottom: number;
+    face: number;
+    hair: number;
+    animal: string;
+    rightStore: string;
+    leftStore: string;
+}
