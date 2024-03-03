6f347b70ce88d3c290300f577e9cf58260dbf904
minseong0324 <kwan0324@naver.com>
2024-03-04 08:29:26 +0900

    Feat: animal export

---
+++ src/assets/animal/index.ts
@@ -0,0 +1,11 @@
+import Cat1 from './cat1.png';
+import Cat2 from './cat2.png';
+import Cat3 from './cat3.png';
+import Cat4 from './cat4.png';
+
+export const animal = [
+    { imgSrc: Cat1, index: 1, width: 60, height: 60, name: 'cat1' },
+    { imgSrc: Cat2, index: 2, width: 60, height: 60, name: 'cat2' },
+    { imgSrc: Cat3, index: 3, width: 60, height: 60, name: 'cat3' },
+    { imgSrc: Cat4, index: 4, width: 60, height: 60, name: 'cat4' },
+]
--- src/assets/skin/index.ts
+++ src/assets/skin/index.ts
@@ -8,23 +8,27 @@ import { WomanHair } from "./woman/hair";
 import { WomanTop } from "./woman/top";
 import { ManBody } from "./man/body";
 import { WomanBody } from "./woman/body";
-
+import { rightStore } from "./rightStore";
+import { leftStore } from "./leftStore";
+import { animal } from "@/assets/animal";
 export const skins = {
     man: [
         { type: 'Face', items: ManFace, title: '얼굴'},
         { type: 'Hair', items: ManHair, title: '머리'},
         { type: 'Top', items: ManTop, title: '상의'},
         { type: 'Bottom', items: ManBottom, title: '하의' },
-        { type: 'animal', items: , title: '고양이'},
-        { type: 'rightStore', items: , title: '가게(오)'},
-        { type: 'leftStore', items: , title: '가게(왼)'},
-
+        { type: 'animal', items: animal, title: '고양이'},
+        { type: 'rightStore', items: rightStore, title: '가게(오)'},
+        { type: 'leftStore', items: leftStore, title: '가게(왼)'},
     ],
     woman: [
         { type: 'Face', items: WomanFace, title: '얼굴'},
         { type: 'Hair', items: WomanHair, title: '머리'},
         { type: 'Top', items: WomanTop, title: '상의'},
         { type: 'Bottom', items: WomanBottom, title: '하의'},
+        { type: 'animal', items: animal, title: '고양이'},
+        { type: 'rightStore', items: rightStore, title: '가게(오)'},
+        { type: 'leftStore', items: leftStore, title: '가게(왼)'},
     ],
 };
 
