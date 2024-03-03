ed0fd03f1389c3d203faaf86cbfb0f697fb781f6
minseong0324 <kwan0324@naver.com>
2024-03-04 06:11:11 +0900

    Fix: sex recoil 관리 통합

--- src/components/Home/Skin/SkinModal/index.tsx
+++ src/components/Home/Skin/SkinModal/index.tsx
@@ -6,23 +6,24 @@ import { SkinSelector } from '../SkinSelector';
 import { skins } from '@/assets/character'; // 스킨 데이터 경로 확인
 import { SkinModalProps } from '@/interfaces/modal';
 import * as Styled from './style';
-import ShortButton from '@/components/ShortButton';
 import SexButton from '@/components/Home/Skin/SexButton';
 import LongButton from '@/components/LongButton';
 import BackButton from '@/components/BackButton';
 
 function SkinModal({ isOpen, onClose }: SkinModalProps) {
     const [skin, setSkin] = useRecoilState(skinState);
-    const [selectedSex, setSelectedSex] = useState('man');
 
     const onSelectSkin = useCallback((skinType, selectedSkin) => {
         setSkin({ ...skin, [skinType]: selectedSkin });
+        console.log('selectedSkin',selectedSkin)
     }, [skin, setSkin]);
 
     const skinStatus = useCallback((type, index) => {
       // 잠금 상태 확인 로직
         return 'locked';
     }, []);
+
+    console.log('skin',skin)
 
     const handleSelectComplete = () => {
         //TODO: mutate해서 서버에 스킨 정보 업데이트. skin값 보내면 됨.
@@ -33,11 +34,11 @@ function SkinModal({ isOpen, onClose }: SkinModalProps) {
             <BackButton onClick={onClose} />
             <Styled.Wrapper>
                 <Styled.RowContainer>
-                    <SexButton selectedSex="man" onClick={() => setSelectedSex('man')} />
-                    <SexButton selectedSex="woman" onClick={() => setSelectedSex('woman')}/>
+                    <SexButton selectedSex="man" onClick={() => setSkin(prevState => ({ ...prevState, sex: "man" }))} />
+                    <SexButton selectedSex="woman" onClick={() => setSkin(prevState => ({ ...prevState, sex: "woman" }))}/>
                 </Styled.RowContainer>
                 <Styled.InnerWrapper>
-                    {skins[selectedSex].map(({ type, items, title }) => (
+                    {skins[skin.sex].map(({ type, items, title }) => (
                     <React.Fragment key={type}>
                     <Styled.SkinTitle>{title}</Styled.SkinTitle>
                     <SkinSelector
