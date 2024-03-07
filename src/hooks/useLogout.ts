b5691f60194dc27c3c7ef7f2497ed814b3e0c1ad
minseong0324 <kwan0324@naver.com>
2024-03-08 04:39:51 +0900

    Feat: 로그아웃 api

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,35 +1,30 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postLogout } from '@/apis/logout';
-import useModal from '@/hooks/useModal';
 import { isAxiosError } from 'axios';
+import useToast from '@/hooks/useToast';
 
 export const useLogout = () => {
-    //const { closeModal } = useModal();
     const queryClient = useQueryClient();
+    const { displayToast } = useToast();
 
     return useMutation({
         mutationFn: () => postLogout(),
         onSuccess: async () => {
             // 로그아웃 성공 시 처리
             await queryClient.invalidateQueries({queryKey: ['userInfo']});
-            //closeModal();
-            // setAlertModal({
-            // isOpen: true,
-            // message: '로그아웃되었습니다.',
-            // });
+            displayToast('로그아웃되었어요.');
+
             // 기타 성공 시 처리
             console.log('로그아웃되었습니다.')
         },
         onError: (error) => {
             // 로그아웃 실패 시 처리
             if (isAxiosError(error)) {
-            // setAlertModal({
-            //     isOpen: true,
-            //     message: '로그아웃 실패했습니다.\n다시 시도해주세요.',
-            // });
+                displayToast('세션이 만료되었어요. 다시 로그인해주세요');
+
             }
             // 기타 에러 처리
-            console.log('로그아웃 실패.')
+            displayToast('세션이 만료되었어요. 다시 로그인해주세요');
         },
     });
 };
