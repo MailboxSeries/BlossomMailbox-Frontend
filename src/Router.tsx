78ba91a52256f5c25cef81e8834ad9ed1e3856b9
minseong0324 <kwan0324@naver.com>
2024-03-09 06:51:36 +0900

    Fix: myId

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,9 @@
 import { Route, Routes} from 'react-router-dom';
 import Home from '@/pages/Home';
 import SignIn from '@/pages/SignIn';
-import { usePostMyId } from '@/hooks/usePostMyId';
-import { useEffect } from 'react';
 
 export default function Router() {
-  const { mutate } = usePostMyId();
 
-  useEffect(() => {
-    mutate();
-  }, []) 
-  
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
