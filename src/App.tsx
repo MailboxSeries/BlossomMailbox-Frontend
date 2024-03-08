b4faea98c1a5a42ae5a8ea9576ec1d7f49b308a4
minseong0324 <kwan0324@naver.com>
2024-03-09 06:56:56 +0900

    Fix: issue

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,12 @@ import Splash from '@/components/common/Splash';
 import { useRecoilValue } from 'recoil';
 import { showSplashState } from '@/atoms/showSplashState';
 import ToastContainer from '@/components/common/ToastContainer';
-import { usePostMyId } from '@/hooks/usePostMyId';
-import { useEffect } from 'react';
 
 const App = () => {
   useScrollToTop();
   const showSplash = useRecoilValue(showSplashState); //TODO: 데이터 패치 성공 시 true로 바뀌어야함
-  const { mutate } = usePostMyId();
 
-  useEffect(() => {
-    mutate();
-  }, [])
   return (
-    <ErrorBoundary fallback={<Splash />}>
       <Suspense fallback={<Splash showSplash={true}/>}>
         <ThemeProvider theme={theme}>
           <BrowserRouter>
@@ -30,7 +23,6 @@ const App = () => {
         </ThemeProvider>
         <ToastContainer />
       </Suspense> 
-    </ErrorBoundary>
   );
 };
 
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,16 @@
 import { Route, Routes} from 'react-router-dom';
 import Home from '@/pages/Home';
 import SignIn from '@/pages/SignIn';
+import { usePostMyId } from '@/hooks/usePostMyId';
+import { useEffect } from 'react';
 
 export default function Router() {
+  const { mutate } = usePostMyId();
 
+  useEffect(() => {
+    mutate();
+  }, [])
+   
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
