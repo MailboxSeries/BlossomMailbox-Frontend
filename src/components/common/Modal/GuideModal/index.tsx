6f3306f6e5de63f7d6c4e9c7f4203f6cda38db80
minseong0324 <kwan0324@naver.com>
2024-03-09 03:43:39 +0900

    Feat: 이용안내 모달

--- src/components/common/Modal/GuideModal/index.tsx
+++ src/components/common/Modal/GuideModal/index.tsx
@@ -4,12 +4,61 @@ import Modal from '@/components/common/Modal';
 import { GuideModalProps } from '@/interfaces/modal';
 
 function GuideModal({onClose, isOpen}: GuideModalProps) {
+    
+    const handleNavigateInstagram = () => { //TODO: 링크 수정
+        window.location.href = 'https://instagram.com/snowmailbox_official?igshid=NzZlODBkYWE4Ng%3D%3D&utm_source=qr';
+    }
 
     return (
         <Modal isOpen={isOpen} onClose={onClose} modalTitle={'이용안내'} modalType={'Modal'}>
             <Styled.Wrapper>
                 <Styled.InnerWrapper>
-                    
+                    <Styled.TextWrapper>
+                        <Styled.DiscriptionText>
+                        {"벚꽃우편함 이용방법 \n"}
+                        </Styled.DiscriptionText>
+                        <Styled.Text>
+                        {"-내 벚꽃 공원을 만들고 지인들에게 링크를 공유해요."}
+                        </Styled.Text>
+                        <Styled.Text>
+                        {"-매일매일 출석을 하며 스킨을 획득해 공원과 나를 더 멋지게 꾸며요."}
+                        </Styled.Text>
+                        <Styled.Text>
+                        {"-지인에게 봄날의 따뜻한 편지와 추억이 담긴 사진을 받을 수 있어요."}
+                        </Styled.Text>
+                        <Styled.Text>
+                        {"-자신에게 편지를 보낸 익명의 누군가와 함께 답장을 주고받을 수 있게 되었어요."}
+                        </Styled.Text>
+
+                        <Styled.DiscriptionText>
+                        {"벚꽃우편함 인스타그램 공식계정\n"}
+                        </Styled.DiscriptionText>
+                        <Styled.InstagramButton onClick={handleNavigateInstagram}>
+                        @blossommailbox_official
+                        </Styled.InstagramButton>
+                        <Styled.Text>
+                        {"-인스타그램 공식계정에서 공지사항을 확인해주세요!"}
+                        </Styled.Text>
+                        <Styled.Text>
+                        {"-불편사항/건의사항 등이 있다면 인스타그램 공식계정을 통해 문의 부탁드려요."}
+                        </Styled.Text>
+
+                        <Styled.DiscriptionText>
+                        {"우편함 Series\n"}
+                        </Styled.DiscriptionText>
+                        <Styled.Text>
+                        {"-단풍우편함, 눈꽃우편함에 이어 벚꽃 시즌을 위한 서비스에요."}
+                        </Styled.Text>
+                        <Styled.Text>
+                        {"-봄에 이어 여름에도 출시 예정이니 많은 기대 부탁드려요!"}
+                        </Styled.Text>
+                        <Styled.Text>
+                        {"-벚꽃우편함은 동국대, 삼육대 학생 5명에서 만든 벚꽃 시즌 서비스예요."}
+                        </Styled.Text>
+                        <Styled.Text>
+                        {"-벚꽃우편함은 비영리 서비스예요."}
+                        </Styled.Text>
+                    </Styled.TextWrapper>
                 </Styled.InnerWrapper>
             </Styled.Wrapper>
         </Modal>
--- src/components/common/Modal/GuideModal/style.ts
+++ src/components/common/Modal/GuideModal/style.ts
@@ -10,47 +10,89 @@ export const Wrapper = styled.div`
   align-items: center;
 `;
 
-export const ButtonWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  margin-top: 70px;
-  width: 230px;
-`;
-
-interface OrnamentButtonProps {
-  image: string;
-};
-
-export const DayButton = styled.button<OrnamentButtonProps>`
-  font-family: 'NanumBarunpenB';
-  font-weight: bolder;
-  text-align: center;
-  font-size: 18px;
-  line-height: 1;
-  color: ${theme.colors.textMain};
-  position: relative;
-  background: url(${props => props.image}) no-repeat center;
-  background-size: 45px 45px;
-  border: none;
-  cursor: pointer;
-  width: 45px;
-  height: 45px;
-  z-index: 3;
-  //padding-right: 25px;
-  padding-top: 3px;
-  margin: 12px;
-  display: flex;
-  justify-content: center; // 가로 중앙 정렬
-  align-items: center; // 세로 중앙 정렬
-`;
-
 export const InnerWrapper = styled.div`
   width: 100%;
-  margin-top: 45px;
-  padding-top: 230px;
+  padding-top: 55px;
   overflow-y: auto;
   display: flex;
   justify-content: center; // 가로 중앙 정렬
   align-items: center; // 세로 중앙 정렬
+`;
+
+export const ButtonText = styled.span`
+  color: ${theme.colors.textMain};
+  text-align: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 1px;
+`;
+
+export const Title = styled.span`
+  color: ${theme.colors.white};
+  font-size: 28px;
+  font-weight: 700;
+  //margin-top: 10px;
+  font-family: 'BareunHipi';
+`;
+
+export const SubTitle = styled.span`
+  color: ${theme.colors.white};
+  font-size: 12px;
+  font-weight: 700;
+  margin-bottom: -5px;
+`;
+
+export const DiscriptionText = styled.span`
+  color: ${theme.colors.white};
+  text-align: center;
+  line-height: 1.8;
+  font-size: 14px;
+    font-style: normal;
+  font-weight: 700;
+  white-space: pre-line;
+  padding-top: 10px;
+`;
+
+
+export const TextWrapper = styled.div`
+  width: 230px;
+`;
+
+export const Text = styled.span`
+    margin-top: 0px;
+    margin-left: 5px;
+    font-family: 'BareunHipi';
+    font-size: 14px;
+    padding-right: 0px;
+    padding-left: 0px;
+    margin-left: 7px;
+    margin-top: -4px;
+    margin-bottom: 2px;
+    white-space: pre-line;
+    line-height: 1.5;
+    display: flex;
+    font-weight: bolder;
+`;
+
+export const InstagramButton = styled.button`
+  background:  ${theme.colors.babyPink};
+  background-size: 180px 36px;
+  z-index: 2;
+  width:180px;
+  height:36px;
+  margin-left: 5px;
+  margin-top: 2px;
+  margin-bottom: 5px;
+  font-family: 'NanumBarunPenB';
+  align-items:center;
+  border: transparent;
+  border-radius: 10px;
+  font-size: 16px;
+  color : ${theme.colors.white};
+  &:active {
+    background-color: ${theme.colors.pink};  // 반투명한 회색
+    border-radius: 10px;
+  }
 `;
