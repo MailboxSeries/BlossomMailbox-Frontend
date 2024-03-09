a31b95a2b1e74102f6b59083eac34f397700541d
minseong0324 <kwan0324@naver.com>
2024-03-10 08:36:38 +0900

    Remove: 불필요 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,14 @@ import {Suspense} from 'react';
 import { ErrorBoundary } from "react-error-boundary";
 import useScrollToTop from '@/hooks/useScrollToTop';
 import Splash from '@/components/common/Splash';
-import { useRecoilValue } from 'recoil';
-import { showSplashState } from '@/atoms/showSplashState';
 import ToastContainer from '@/components/common/ToastContainer';
 
 const App = () => {
   useScrollToTop();
-  const showSplash = useRecoilValue(showSplashState); //TODO: 데이터 패치 성공 시 true로 바뀌어야함
 
   return (
     <ErrorBoundary fallback={<Splash showSplash={false}/>}>
-      <Suspense fallback={<Splash showSplash={true}/>}>
+      <Suspense fallback={<Splash showSplash={false}/>}>
         <ThemeProvider theme={theme}>
           <BrowserRouter>
               <Router />
