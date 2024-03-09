2693ec57b64dd7d57d5f8d6f611b91dddcbca22f
minseong0324 <kwan0324@naver.com>
2024-03-10 05:47:08 +0900

    Fix: isMyHome 분기처리 추가

--- src/hooks/useIsMyHome.ts
+++ src/hooks/useIsMyHome.ts
@@ -7,7 +7,7 @@ export default function useIsMyHome() {
   const query = new URLSearchParams(location.search);
   const ownerId = query.get('u'); // 'u'는 URL에서 ownerId 값을 담고 있는 질의 매개변수의 이름입니다.
   const myId = useRecoilValue(myIdState);
-  const isMyHome = (ownerId) === myId; // 지금 위치가 나의 집인지?
+  const isMyHome = (ownerId) === myId && location.pathname !== '/'; // 지금 위치가 나의 집인지?
   
   return {ownerId, myId, isMyHome};
 }
