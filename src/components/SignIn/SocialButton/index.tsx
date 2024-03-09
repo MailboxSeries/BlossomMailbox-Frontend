15d22df6bb0f9568e1ee80a7632a9618eba5248d
minseong0324 <kwan0324@naver.com>
2024-03-10 08:30:56 +0900

    Fix: 네이버 로그인 막기

--- src/components/SignIn/SocialButton/index.tsx
+++ src/components/SignIn/SocialButton/index.tsx
@@ -1,12 +1,18 @@
 import Button from '@/components/common/Button';
+import useToast from '@/hooks/useToast';
 import { SocialButtonProps } from '@/interfaces/button';
 import { getSocialSignInImage } from '@/utils/getSocialSignInImage';
 
 export default function SocialButton(props: SocialButtonProps) {
   const socialImg = getSocialSignInImage(props.socialType);
+  const { displayToast } = useToast();
 
   const handleSignIn = () => {
-    window.location.href = `https://api.blossommailbox.com/oauth2/authorization/${props.socialType}`;
+    if(props.socialType === 'naver') {
+      displayToast('네이버 로그인은 2일 뒤에 사용가능해요.');
+    } else {
+      window.location.href = `https://api.blossommailbox.com/oauth2/authorization/${props.socialType}`;
+    }
   };
 
   return (
