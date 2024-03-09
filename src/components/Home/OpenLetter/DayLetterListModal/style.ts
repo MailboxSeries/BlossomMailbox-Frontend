7dd7def74dad5fef20eb867d47a8ae3841d8200b
minseong0324 <kwan0324@naver.com>
2024-03-10 04:11:18 +0900

    Design: 높이 변경

--- src/components/Home/OpenLetter/DayLetterListModal/style.ts
+++ src/components/Home/OpenLetter/DayLetterListModal/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 98%;
+  height: 98.2%;
   display: flex;
   flex-direction: column;
   align-items: center;
